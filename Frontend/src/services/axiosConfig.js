56264079fd548b9d83dd14af07a78e24190e4c0e
SupriyaKumari <160938915+SupriyaRathore12@users.noreply.github.com>
2026-03-03 16:09:01 +0530

    Update axios base URL for the service

--- Frontend/src/services/axiosConfig.js
+++ Frontend/src/services/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance=axios.create({
-    baseURL:"https://ecommerce-fullstack-backend-7p2q.onrender.com",
+    baseURL:"https://ecommerce-fullstack-snjq.onrender.com",
     headers:{
         "Content-Type":"application/json",
     }
